cbe323598330366c0ccde21f8e171c0a5bd4714d
Daria1909 <timohina.daria@gmail.com>
2020-11-07 11:09:48 +0300

    Program

---
+++ Программа.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Задача
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i, j, k = 0;
+            int[] arr = new int[N];
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < arr.Length - 1; i++)
+            {
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] * arr[j] % 26 == 0)
+                    {
+                        k += 1;
+                    }
+                }
+            }
+            Console.WriteLine(k);
+        }
+    }
+}
