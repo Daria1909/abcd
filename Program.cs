9175ee909fc12c795290dee03ceda55562d69106
Daria1909 <timohina.daria@gmail.com>
2020-12-11 19:54:07 +0300

    Array printing conflict resolved.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                int N = Convert.ToInt32(Console.ReadLine());
+                int i, j, k = 0;
+                int[] arr = new int[N];
+
+                for (i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (i = 0; i < arr.Length - 1; i++)
+                {
+                    for (j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[i] * arr[j] % 26 == 0)
+                        {
+                            k += 1;
+                        }
+                    }
+                }
+                Console.WriteLine(k);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено нечисловое значение!");
+            }
+        }
+    }
+}
